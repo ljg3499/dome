57991bd2eaa37d56ea92b00da70f96d858951418
李锦贵 <1843018044@qq.com>
2020-03-14 16:19:10 +0800

    测试

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img class="logo" src="https://tva1.sinaimg.cn/large/006tNbRwgy1gb0uax9igwj31400u00vl.jpg">
+    <!-- <HelloWorld/> -->
+    <h1>我命由我不由天</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.logo {
+  width: 100px;
+  height: 100px;
+}
+</style>
